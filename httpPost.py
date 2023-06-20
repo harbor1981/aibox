e6a55b72519d0511f570c1b181d9fcc4f3e77eb1
harbor1981 <3993770@qq.com>
2023-06-20 15:38:58 +0800

    add some codes

---
+++ httpPost.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def post_result(Vehicle_count=0, Person_count=0, callback_url='http://199.19.110.7:7104/api/Callback'):
+    # post_url = 'http://199.19.110.7:7104/api/Callback'
+    # 创建包含要发送的参数的字典
+    message = {
+        'Vehicle_count': Vehicle_count,
+        'Person_count': Person_count
+    }
+    # message=json.dumps(message)
+    response = requests.post(callback_url, json=message, headers={'content-type': 'application/json;charset=UTF-8'})
+    headers = {'Content-Type': 'application/json;charset=UTF-8'}
+    # response = requests.request("post",post_url, json=message, headers=headers)
+    # 检查响应状态码
+    if response.status_code == 200:
+        print('请求已成功发送')
+    else:
+        print('请求失败')
+    print(response.text)
+
+if __name__ == '__main__':
+    post_result(10,100,'http://192.168.1.11:5002/process_json')
+
---
+++ park/utils.py
@@ -0,0 +1,102 @@
+import socket
+import os
+import signal
+import string
+import random
+
+import numpy as np
+import psutil as psutil
+
+
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+def killProcesses(*pids):
+  for pid in pids:
+    a = os.kill(pid, signal.SIGKILL)
+    print('已杀死pid为%s的进程,　返回值是:%s' % (pid, a))
+
+def get_pid(*ports):
+	#其中\"为转义"
+  pids = []
+  for port in ports:
+    pid = os.popen("netstat -nlp | grep :%s | awk '{print $7}' | awk -F\" / \" '{ print $1 }'" % (port)).read().split('/')[0]
+    if pid:
+      pids.append(int(pid))
+  return pids
+def killProcessByName(processName):
+    pids = psutil.pids()
+    for pid in pids:
+        p = psutil.Process(pid)
+        # get process name according to pid
+        process_name = p.name()
+        if process_name.find(processName)!=-1:
+            print("Process name is: %s, pid is: %s" % (process_name, pid))
+            os.kill(pid, signal.SIGKILL)  
+
+def killProcessByPort(port):
+    code='200'
+    msg='success'
+    data='data'
+    if port==0:
+        print("111111111111111111111111111111111")
+        for i in range(30):
+            command="kill -9 $(netstat -nlp | grep :"+str(9600+i)+" | awk '{print $7}' | awk -F'/' '{{ print $1 }}')"
+            print(command)
+            os.system(command)
+        msg="all processes have been killed"
+        return code,msg,data
+    elif 9600<=port and port<9630:
+        '''root authority is required'''
+        command="kill -9 $(netstat -nlp | grep :"+str(port)+" | awk '{print $7}' | awk -F'/' '{{ print $1 }}')"
+        os.system(command)
+        msg='%s has been killed'%(port)
+        return code,msg,data
+    else:
+        code='-1'
+        msg='error!port must in (9600,9630)'
+        return code,msg,data
+
+def generateFiles(nums):
+    a = 1
+    while (a <= nums):
+        fwrite = str(a) + "git submodule update --init的使用方式git submodule update --init的使用方式 \n"
+        file = open("/home/quejl/files/" + str(a) + ".txt", "a")
+        f = 0
+        while (f <= 100):
+            f += 1
+            file.write(fwrite)
+        file.close()
+        a += 1
+def generateBigFile():
+    n = 1024 ** 2  # 1 Mb of text
+    letters = np.array(list(chr(ord('a') + i) for i in range(26)))
+    chars = np.random.choice(np.fromstring(letters, dtype='<U1'), n)
+    with open('/home/quejl/textfile111.txt', 'w+') as f:
+        f.write(''.join(chars))
+def generateBigFiles(nums,filePath):
+    a = 1
+    letters = np.array(list(chr(ord('a') + i) for i in range(26)))
+    while (a <= nums):
+        n = 100 ** 2*random.randint(1,500)  # 0.1Mb-5Mb of text
+        chars = np.random.choice(np.fromstring(letters, dtype='<U1'), n)
+        with open(filePath + str(a) + ".txt", 'w+') as f:
+            f.write(''.join(chars))
+        a += 1
+
+
+if __name__ == '__main__':
+    code,msg,message=killProcessByPort(-1)
+    print("code=%s,msg=%s,data=%s" % (code, msg, message))
+    # killProcessByName("alibaba")
+    # generateBigFiles(100,"/home/quejl/files2/")
+    # generateBigFile()
+    # killOneProcess(5000)
+    # out_port=5000
+    # ps_ports = [out_port]
+    # killProcesses(*get_pid(*ps_ports))
